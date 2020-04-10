d5585e133f4f1e618e1d9fa4fc21750207ed7b57
jimothyji <jimm.ji03@gmail.com>
2020-04-09 21:25:25 -0400

    your cool

--- WeebChronicles.java
+++ WeebChronicles.java
@@ -7,7 +7,7 @@ import javax.imageio.*;
 import javax.sound.midi.*;
 import java.applet.*;
 
-//yo
+//yo boobs
 
 public class WeebChronicles extends JFrame implements ActionListener {
 	Timer myTimer;
