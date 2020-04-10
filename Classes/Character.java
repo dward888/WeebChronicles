e4f3d6d6978c914d441ada520b05bd87c73d2c14
jimothyji <jimm.ji03@gmail.com>
2020-04-09 23:20:09 -0400

    character class

--- Classes/Character.java
+++ Classes/Character.java
@@ -1,27 +1,28 @@
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.*;
-import javax.swing.*;
+
 
 public class Character {
     private String name;
     private int attack, defense, speed;
-    private Image[]run;
+    private Image[]runRight;
+    private Image[]runLeft;
 
     public Character(String n, int a,  int d, int s){
         name = n;
         attack = a;
         defense = d;
         speed = s;
-        run  = new Image[6];
+        runRight  = new Image[6];
+        runLeft = new Image[6];
     }
-    public void loadSprite(Image[]action, String motion, String directory) {
+    public void loadSprite(Image[]actionRight, Image[]actionLeft, String motion, String directory) {
         try{
-            for(int i = 0; i < action.length; i++) {
+            for(int i = 0; i < actionRight.length; i++) {
                 Image img = ImageIO.read(new File(directory + motion + i + ".gif"));
-                action[i] = img;
+                actionRight[i] = img;
             }
         }
         catch(IOException e ){
