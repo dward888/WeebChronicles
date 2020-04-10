8d34f24d56259ee9357f02397b1addb320f8ebbf
jimothyji <jimm.ji03@gmail.com>
2020-04-09 22:48:09 -0400

    character class

--- Classes/Character.java
+++ Classes/Character.java
@@ -1,2 +1,14 @@
+import javax.imageio.*;
+import javax.swing.*;
+
 public class Character {
+    private String name;
+    private int attack, defense, speed;
+
+    public Character(String n, int a,  int d, int s){
+        name = n;
+        attack = a;
+        defense = d;
+        speed = s;
+    }
 }
