4c0c581ee0d23c169f51241e7effdb2226a4ea14
Edward <edwardcentral@gmail.com>
2020-06-06 15:16:17 -0400

    made attacking a lil easier

--- Classes/Player.java
+++ Classes/Player.java
@@ -143,10 +143,10 @@ public class Player{
 	}
 
 	public Rectangle getRHitRect(){
-    	return new Rectangle(x+5,y+12,80,45);
+    	return new Rectangle(x+5,y+12,100,45);
 	}
 	public Rectangle getLHitRect(){
-    	return new Rectangle(x-35,y+12,80,45);
+    	return new Rectangle(x-50,y+12,80,45);
 	}
 
 	public int getLives() {
